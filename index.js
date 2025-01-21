19ee8279a843c181e967839b9eae07a6403cea4f
ShivrajM <S@M.com>
2025-01-21 21:09:56 +0530

    Index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('All Done Dostoo!');
+ res.send('COMPLETED BY GITBASH!');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
